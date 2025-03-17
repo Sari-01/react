a66c7340ffd8f4156272f2b91e6e429fa80c6c20
Sari-01 <sariha9601@gmail.com>
2025-03-17 14:00:28 +0530

    Add files via upload

---
+++ src/component/Headers.js
@@ -0,0 +1,17 @@
+import './Headers.css'
+import styles from './Headers.module.css'
+
+
+function Headers(){
+    return(
+        <>
+        <h1 className={styles.bigBlue}>Heyyyy</h1>
+        <p>This is cool!</p>
+        </>
+    )
+}
+
+export default Headers;
+
+
+
