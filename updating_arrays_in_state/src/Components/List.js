1feda9ffec320a1ba66f007b4e83304b6f77c63e
Sari-01 <sariha9601@gmail.com>
2025-03-17 14:44:03 +0530

    Add files via upload

---
+++ updating_arrays_in_state/src/Components/List.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function List(){
+    const [list,setList]=useState([])
+    const [count,setCount]=useState(1)
+    function addItem(){
+        console.log("yesss")
+        const itemName='item'+count
+        setList((previousState)=>{return [...previousState,itemName]})
+        setCount((previousState)=>{return previousState+1})
+    }
+    console.log(list)
+    return(
+    <>
+    <h1>Lists</h1>
+    <button onClick={addItem}>Add Item</button>
+    <ul>{
+        list.map((el,index)=> <li key={index}>{el}</li>)
+        }
+    </ul>
+    </>)
+}
+
+export default List;
